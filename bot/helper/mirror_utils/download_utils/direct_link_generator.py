7b53b6513a9e1686e194c149182adf6398434b57
projectninjatech <87536062+projectninjatech@users.noreply.github.com>
2021-10-19 17:14:03 +0530

    Add files via upload

---
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -0,0 +1,194 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Helper Module containing various sites direct links generators. This module is copied and modified as per need
+from https://github.com/AvinashReddy3108/PaperplaneExtended . I hereby take no credit of the following code other
+than the modifications. See https://github.com/AvinashReddy3108/PaperplaneExtended/commits/master/userbot/modules/direct_links.py
+for original authorship. """
+
+from bot import UPTOBOX_TOKEN
+import logging
+import json
+import math
+import re
+import urllib.parse
+from os import popen
+from random import choice
+
+import requests
+from bs4 import BeautifulSoup
+
+from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
+
+
+def direct_link_generator(link: str):
+    """ direct links generator """
+    if not link:
+        raise DirectDownloadLinkException("`No links found!`")
+    elif 'zippyshare.com' in link:
+        return zippy_share(link)
+    elif 'yadi.sk' in link:
+        return yandex_disk(link)
+    elif 'cloud.mail.ru' in link:
+        return cm_ru(link)
+    elif 'mediafire.com' in link:
+        return mediafire(link)
+    elif 'uptobox.com' in link:
+        return uptobox(link)
+    elif 'osdn.net' in link:
+        return osdn(link)
+    elif 'github.com' in link:
+        return github(link)
+    else:
+        raise DirectDownloadLinkException(f'No Direct link function found for {link}')
+
+""" Zippy-Share up-to-date plugin from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/zippyshare.py """
+""" Thanks to all contributors @aryanvikash, rking32, @BianSepang """
+
+link = r'https://www(\d{1,3}).zippyshare.com/v/(\w{8})/file.html'
+regex_result = (
+    r'var a = (\d{6});\s+var b = (\d{6});\s+document\.getElementById'
+    r'\(\'dlbutton\'\).omg = "f";\s+if \(document.getElementById\(\''
+    r'dlbutton\'\).omg != \'f\'\) {\s+a = Math.ceil\(a/3\);\s+} else'
+    r' {\s+a = Math.floor\(a/3\);\s+}\s+document.getElementById\(\'d'
+    r'lbutton\'\).href = "/d/[a-zA-Z\d]{8}/\"\+\(a \+ \d{6}%b\)\+"/('
+    r'[\w%-.]+)";'
+)
+
+def zippy_share(url: str) -> str:
+    session = requests.Session()
+    with session as ses:
+        match = re.match(link, url)
+        if not match:
+            raise ValueError("Invalid URL: " + str(url))
+        server, id_ = match.group(1), match.group(2)
+        res = ses.get(url)
+        res.raise_for_status()
+        match = re.search(regex_result, res.text, re.DOTALL)
+        if not match:
+            raise ValueError("Invalid Response!")
+        val_1 = int(match.group(1))
+        val_2 = math.floor(val_1 / 3)
+        val_3 = int(match.group(2))
+        val = val_1 + val_2 % val_3
+        name = match.group(3)
+        dl_url = "https://www{}.zippyshare.com/d/{}/{}/{}".format(server, id_, val, name)
+    return dl_url
+
+
+def yandex_disk(url: str) -> str:
+    """ Yandex.Disk direct links generator
+    Based on https://github.com/wldhx/yadisk-direct"""
+    try:
+        link = re.findall(r'\bhttps?://.*yadi\.sk\S+', url)[0]
+    except IndexError:
+        reply = "`No Yandex.Disk links found`\n"
+        return reply
+    api = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
+    try:
+        dl_url = requests.get(api.format(link)).json()['href']
+        return dl_url
+    except KeyError:
+        raise DirectDownloadLinkException("`Error: File not found / Download limit reached`\n")
+
+
+def cm_ru(url: str) -> str:
+    """ cloud.mail.ru direct links generator
+    Using https://github.com/JrMasterModelBuilder/cmrudl.py"""
+    reply = ''
+    try:
+        link = re.findall(r'\bhttps?://.*cloud\.mail\.ru\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No cloud.mail.ru links found`\n")
+    command = f'vendor/cmrudl.py/cmrudl -s {link}'
+    result = popen(command).read()
+    result = result.splitlines()[-1]
+    try:
+        data = json.loads(result)
+    except json.decoder.JSONDecodeError:
+        raise DirectDownloadLinkException("`Error: Can't extract the link`\n")
+    dl_url = data['download']
+    return dl_url
+
+def uptobox(url: str) -> str:
+    """ Uptobox direct links generator
+    based on https://github.com/jovanzers/WinTenCermin """
+    try:
+        link = re.findall(r'\bhttps?://.*uptobox\.com\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No Uptobox links found`\n")
+    if UPTOBOX_TOKEN is None:
+        logging.error('UPTOBOX_TOKEN not provided!')
+        dl_url = url
+    else:
+        try:
+            link = re.findall(r'\bhttp?://.*uptobox\.com/dl\S+', url)[0]
+            logging.info('Uptobox direct link')
+            dl_url = url
+        except:
+            file_id = re.findall(r'\bhttps?://.*uptobox\.com/(\w+)', url)[0]
+            file_link = 'https://uptobox.com/api/link?token=%s&file_code=%s' % (UPTOBOX_TOKEN, file_id)
+            req = requests.get(file_link)
+            result = req.json()
+            dl_url = result['data']['dlLink']
+    return dl_url
+
+
+def mediafire(url: str) -> str:
+    """ MediaFire direct links generator """
+    try:
+        link = re.findall(r'\bhttps?://.*mediafire\.com\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No MediaFire links found`\n")
+    page = BeautifulSoup(requests.get(link).content, 'lxml')
+    info = page.find('a', {'aria-label': 'Download file'})
+    dl_url = info.get('href')
+    return dl_url
+
+
+def osdn(url: str) -> str:
+    """ OSDN direct links generator """
+    osdn_link = 'https://osdn.net'
+    try:
+        link = re.findall(r'\bhttps?://.*osdn\.net\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No OSDN links found`\n")
+    page = BeautifulSoup(
+        requests.get(link, allow_redirects=True).content, 'lxml')
+    info = page.find('a', {'class': 'mirror_link'})
+    link = urllib.parse.unquote(osdn_link + info['href'])
+    mirrors = page.find('form', {'id': 'mirror-select-form'}).findAll('tr')
+    urls = []
+    for data in mirrors[1:]:
+        mirror = data.find('input')['value']
+        urls.append(re.sub(r'm=(.*)&f', f'm={mirror}&f', link))
+    return urls[0]
+
+
+def github(url: str) -> str:
+    """ GitHub direct links generator """
+    try:
+        re.findall(r'\bhttps?://.*github\.com.*releases\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No GitHub Releases links found`\n")
+    download = requests.get(url, stream=True, allow_redirects=False)
+    try:
+        dl_url = download.headers["location"]
+        return dl_url
+    except KeyError:
+        raise DirectDownloadLinkException("`Error: Can't extract the link`\n")
+
+
+def useragent():
+    """
+    useragent random setter
+    """
+    useragents = BeautifulSoup(
+        requests.get(
+            'https://developers.whatismybrowser.com/'
+            'useragents/explore/operating_system_name/android/').content,
+        'lxml').findAll('td', {'class': 'useragent'})
+    user_agent = choice(useragents)
+    return user_agent.text
